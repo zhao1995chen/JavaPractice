00be6ebdf15b973f015a13549bb31542a736de72
zhao1995chen <zhao1995.chen@gmail.com>
2020-07-01 16:46:07 +0800

    Add method to use the anonymous function

--- day37/SortArray/src/PointerDemo2.java
+++ day37/SortArray/src/PointerDemo2.java
@@ -8,9 +8,9 @@ public class PointerDemo2 {
 	public static void main(String[] args) {
 		List<MyValue> mv = new ArrayList<>();
 
-		mv.add(new MyValue(1, 3));
+		mv.add(new MyValue(5, 1));
 		mv.add(new MyValue(2, 2));
-		mv.add(new MyValue(3, 1));
+		mv.add(new MyValue(3, 4));
 
 //		for(MyValue m: mv) {
 //			System.out.println(m);
@@ -22,8 +22,19 @@ public class PointerDemo2 {
 		// Comparator need to return integer.
 		// If the datatype of value isn't integer, need to do transformation.
 		// Or do the if-else or  to make the same output
-		Comparator<MyValue> c1 = (p1, p2) -> (int)(p1.v1 - p2.v1);
-		Collections.sort(mv, c1);
+		
+		// Method 1
+//		Comparator<MyValue> c1 = (p1, p2) -> (int)(p1.v1 - p2.v1);
+//		Collections.sort(mv, c1);
+		
+		// Method 2
+		Collections.sort(mv, new Comparator<MyValue>() {
+			public int compare(MyValue p1, MyValue p2) {
+				return Math.sqrt(Math.pow(p1.v1, 2) + Math.pow(p1.v2, 2)) > Math.sqrt(Math.pow(p2.v1, 2) + Math.pow(p2.v2, 2)) ? 
+						1 : Math.sqrt(Math.pow(p1.v1, 2) + Math.pow(p1.v2, 2)) < Math.sqrt(Math.pow(p2.v1, 2) + Math.pow(p2.v2, 2)) ? -1 : 0;
+			}
+		});
+		
 		for(MyValue m: mv) {
 			System.out.println(m);
 		}
@@ -34,7 +45,6 @@ class MyValue {
 	double v1, v2;
 
 	public MyValue(double v1, double v2) {
-		super();
 		this.v1 = v1;
 		this.v2 = v2;
 	}
