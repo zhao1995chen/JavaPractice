a2f0578dc98dda082188dafe242d078f695d6ddc
zhao1995chen <zhao1995.chen@gmail.com>
2020-07-07 11:54:22 +0800

    login page with beans

---
+++ day41/beansDemo/src/beans/LoginBean.java
@@ -0,0 +1,35 @@
+package beans;
+
+public class LoginBean {
+	private String username;
+	private String password;
+	private String phoneNumber;
+	
+	public LoginBean() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
