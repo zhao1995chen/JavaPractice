373cddbf2aad61ca90ae3635ec588ae919af49f0
zhao1995chen <zhao1995.chen@gmail.com>
2020-07-08 15:29:20 +0800

    class MyProduct

---
+++ day42/ShoppingCart/WebContent/pview.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=BIG5"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<p>Name: ${product.name}</p>
+	<p>Price: ${product.price}</p>
+</body>
+</html>
---
+++ day42/ShoppingCart/src/beans/MyProduct.java
@@ -0,0 +1,28 @@
+package beans;
+
+public class MyProduct {
+	private String name;
+	private int price;
+	
+	public MyProduct(String name, int price) {
+		super();
+		setName(name);
+		setPrice(price);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
