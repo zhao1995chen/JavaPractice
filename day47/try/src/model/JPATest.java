ffd3f86ea18d83f8661785b036e0e4c7320fe30c
zhao1995chen <zhao1995.chen@gmail.com>
2020-07-16 16:28:47 +0800

    CRUD

--- day47/try/src/model/JPATest.java
+++ day47/try/src/model/JPATest.java
@@ -18,6 +18,27 @@ public class JPATest {
            System.out.println(user.getEmployeeNumber());
        }
        
+       // Create
+       Employee e = new Employee();
+       e.setEmployeeNumber(1801);
+       e.setEmail("sean@google.com");
+       e.setFirstName("Sean");
+       e.setLastName("Chen");
+       e.setExtension("oxooox1");
+       e.setJobTitle("VP2");
+       e.setOfficeCode("1");
+       entityManager.persist(e);
+       
+       // Need to find first before update & delete
+       // Update
+       Employee e2 = entityManager.find(Employee.class, 1625);
+       e2.setJobTitle("Marketing Manager");
+       entityManager.persist(e2);
+       
+       // Delete
+       Employee e3 = entityManager.find(Employee.class, 1801);
+       entityManager.remove(e3);
+       
        entityManager.getTransaction().commit();
        entityManager.close();
 
